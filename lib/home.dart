2a67e088e4b31f8a7403a21441df0fb12a393f65
Sebastian-Mat <67850496+Sebastian-Mat@users.noreply.github.com>
2021-03-09 14:51:22 -0500

    ADD: Bottom Navigation Bar

---
+++ lib/home.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 2;
+  List<Widget> _widgetOptions = [
+    Text("Pyshics"),
+    Text("Math"),
+    Text("Home"),
+    Text("Geometry"),
+    Text("Text")
+  ];
+
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 90,
+        child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                topRight: Radius.circular(30.0),
+              ),
+              child: BottomNavigationBar(
+                  iconSize: 27,
+                  currentIndex: _selectedIndex,
+                  onTap: _onItemTap,
+                  selectedItemColor: Colors.blue,
+                  unselectedItemColor: Colors.grey,
+                  items: [
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.straighten),
+                      label: "Physics",
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.subscript),
+                      label: "Mathematics",
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.home),
+                      label: "Home",
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.square_foot),
+                      label: "Geometry",
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.text_fields),
+                      label: "Text",
+                    ),
+                  ]),
+            )),
+      ),
+    );
+  }
+}
